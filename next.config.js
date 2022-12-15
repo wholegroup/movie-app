1f1b58d6b24f5d876ce74507093678ee1b3e7346
Andrey Rychkov <jonny@wholegroup.com>
2022-12-15 17:26:45 +0700

    one more sw comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+  // https://github.com/shadowwalker/next-pwa/tree/master/examples/custom-worker
   // https://www.proximity.blog/post/building-a-next-js-pwa-using-nextpwa-and-service-worker-2022330
   // swSrc: 'service-worker.js',
 })
