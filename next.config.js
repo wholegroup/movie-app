4388d67293ab2d7f87b1096cfa6f8ac01ab461e1
Andrey Rychkov <jonny@wholegroup.com>
2022-12-15 17:20:39 +0700

    added sw comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+  // https://www.proximity.blog/post/building-a-next-js-pwa-using-nextpwa-and-service-worker-2022330
+  // swSrc: 'service-worker.js',
 })
 
 /** @type {import('next').NextConfig} */
