decf5c45b24ac82c9531f0518cd5211c14a1ebc6
Andrey Rychkov <jonny@wholegroup.com>
2022-12-20 17:46:24 +0700

    created dump movies service

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -4,17 +4,20 @@ import MovieService from '../../services/MovieService'
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
-export default function handler (req, res) {
+export default async function handler (req, res) {
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
     return
   }
+
+  const lastUpdatedAt = req.body?.lastUpdatedAt || 0
+  const movieService = new MovieService(':memory:')
+
   res.status(200).json({
-    movie: [],
-    votes: [],
-    images: [],
-    test: req.query,
-    lastUpdatedAt: 0
+    movies: await movieService.moviesSince(lastUpdatedAt),
+    votes: await movieService.votesSince(lastUpdatedAt),
+    images: await movieService.imagesSince(lastUpdatedAt),
+    lastUpdatedAt: lastUpdatedAt
   })
 }
--- services/MovieService.js
+++ services/MovieService.js
@@ -1,4 +1,27 @@
 class MovieService {
+  /** @type {string} */
+  filename
+
+  constructor (filename) {
+    this.filename = filename
+  }
+
+  async open () {
+
+  }
+
+  async moviesSince (lastUpdatedAt) {
+    return []
+  }
+
+  async votesSince (lastUpdatedAt) {
+    return []
+  }
+
+  async imagesSince (lastUpdatedAt) {
+    return []
+  }
+
 
 }
 
