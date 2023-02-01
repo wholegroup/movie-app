32955980d798f84b7e14c32fbb19862a7d5ec4a7
Andrey Rychkov <jonny@wholegroup.com>
2023-02-01 13:09:57 +0700

    added bundler analyzer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+}
+
 const withPWA = require('next-pwa')({
-  dest: 'public',
+  dest: 'public'
   // https://github.com/shadowwalker/next-pwa/tree/master/examples/custom-worker
   // https://www.proximity.blog/post/building-a-next-js-pwa-using-nextpwa-and-service-worker-2022330
   // swSrc: 'service-worker.js',
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = withPWA(nextConfig)
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
