ba00cbed30e7826c6b2f6d6e318edd3192ff84ac
Andrey Rychkov <jonny@wholegroup.com>
2023-02-22 15:51:31 +0700

    temporary disable minification

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack (webpackConfig) {
+    return {
+      ...webpackConfig,
+      // disable minification
+      optimization: {
+        minimize: false
+      }
+    }
+  }
 }
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  reloadOnOnline: false, //
+  mode: 'development' // don't minified sw
   // https://github.com/shadowwalker/next-pwa/tree/master/examples/custom-worker
   // https://www.proximity.blog/post/building-a-next-js-pwa-using-nextpwa-and-service-worker-2022330
   // swSrc: 'service-worker.js',
