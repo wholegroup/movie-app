34f12841dabc00a77b1ea0bc65ad769cba41e4ad
Andrey Rychkov <jonny@wholegroup.com>
2023-02-23 11:29:55 +0700

    cleaned images

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,6 @@ function MyApp ({ Component, pageProps }) {
         <title>Annual Movies</title>
 
         <link rel='manifest' href='/manifest.json' />
-        <link href='/icons/icon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
         <link href='/icons/icon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
         <link rel='apple-touch-icon' href='/icons/icon-512x512.png'></link>
         <meta name='theme-color' content='#317EFB' />
