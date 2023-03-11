9c098fc40967aac791a693cff8b52960e27bbefb
Andrey Rychkov <jonny@wholegroup.com>
2023-03-11 15:59:32 +0700

    enabled minification MOV-3

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const nextConfig = {
   reactStrictMode: true,
   webpack (webpackConfig) {
     return {
-      ...webpackConfig,
-      // disable minification
-      optimization: {
-        minimize: false
-      }
+      ...webpackConfig
+      // // disable minification
+      // optimization: {
+      //   minimize: false
+      // }
     }
   }
 }
@@ -20,8 +20,8 @@ const withPWA = nextPWA({
   dest: 'public',
   runtimeCaching,
   dynamicStartUrl: false, // if enabled the app fetches index page every time
-  reloadOnOnline: false, //
-  mode: 'development' // don't minified sw
+  reloadOnOnline: false
+  // mode: 'development' // don't minified sw
   // https://github.com/shadowwalker/next-pwa/tree/master/examples/custom-worker
   // https://www.proximity.blog/post/building-a-next-js-pwa-using-nextpwa-and-service-worker-2022330
   // swSrc: 'service-worker.js',
