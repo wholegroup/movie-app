b695c993c4ac642d6a81caf0dee92502b806c77c
Andrey Rychkov <jonny@wholegroup.com>
2023-04-21 13:33:25 +0700

    converted /home and /account links

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link.js'
 import { Icon } from '@mdi/react'
 import { mdiAccount } from '@mdi/js'
 import styles from './Toolbar.module.css'
@@ -5,7 +6,11 @@ import styles from './Toolbar.module.css'
 function Toolbar () {
   return (
     <nav className={styles.nav}>
-      <div className={styles.first}>v{process.env.NEXT_PUBLIC_MOVIE_VERSION || '00.00.00'}</div>
+      <div className={styles.first}>
+        <Link href='/'>
+          v{process.env.NEXT_PUBLIC_MOVIE_VERSION || '00.00.00'}
+        </Link>
+      </div>
       <div>
         <ul>
           <li><a href='#'>Section A</a></li>
@@ -15,7 +20,9 @@ function Toolbar () {
         </ul>
       </div>
       <div className={styles.last}>
-        <Icon id={'account'} path={mdiAccount} size={1.5} title={'Anonymous'} />
+        <Link href='/account'>
+          <Icon id={'account'} path={mdiAccount} size={1.5} title={'Anonymous'} />
+        </Link>
       </div>
     </nav>
   )
