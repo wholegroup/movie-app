20b9057a937b2ee467655e16a49603f948a2e580
Andrey Rychkov <jonny@wholegroup.com>
2023-05-08 15:36:53 +0700

    updated movie by refresh ts

--- src/components/MovieLoader.js
+++ src/components/MovieLoader.js
@@ -9,12 +9,14 @@ import globalContext from '../context/globalContext.js'
 function MovieLoader ({ slug }) {
   const { commonStore, syncStore } = useContext(globalContext)
 
+  // Load movie by slug
   useEffect(() => {
     async function load () {
       if (!commonStore.isInitialized) {
         return
       }
 
+      await commonStore.setRefreshTs(0)
       await commonStore.loadMovieBySlug(slug)
     }
 
@@ -26,8 +28,26 @@ function MovieLoader ({ slug }) {
       commonStore.setVotes(null)
       commonStore.setImages(null)
       commonStore.setMetadata(null)
+      commonStore.setDetails(null)
     }
   }, [slug, commonStore, commonStore.isInitialized, syncStore?.lastUpdatedAt])
+
+  // Refresh movie by commonStore.refreshTs
+  useEffect(() => {
+    async function refresh () {
+      if (!commonStore.refreshTs) {
+        return
+      }
+
+      await commonStore.loadMovieBySlug(slug)
+    }
+
+    refresh()
+      .catch(console.error)
+
+    return () => {
+    }
+  }, [slug, commonStore, commonStore.refreshTs])
 
   return null
 }
