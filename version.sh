7f7ba1a3e51eb301849cdd61341974b090b92540
Andrey Rychkov <jonny@wholegroup.com>
2023-05-12 10:24:21 +0700

    changed version string

--- version.sh
+++ version.sh
@@ -1,1 +1,5 @@
-echo $(date -u +%y.%m.%d).$(printf "%05d\n" $(date -u "+(%H*60+%M)*60+%S" | bc))
+# like 23.05.12.12177
+#echo $(date -u +%y.%m.%d).$(printf "%05d\n" $(date -u "+(%H*60+%M)*60+%S" | bc))
+
+# like 23.05.12
+echo $(date -u +%y.%m.%d)
