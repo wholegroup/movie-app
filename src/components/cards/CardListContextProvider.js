521a635836792bb2d802afa71252478e9095c97f
Andrey Rychkov <jonny@wholegroup.com>
2023-06-07 13:01:06 +0700

    set car list context value via ref

--- src/components/cards/CardListContextProvider.js
+++ src/components/cards/CardListContextProvider.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import globalContext from '../../context/globalContext.js'
 import cardListContext from './cardListContext.js'
 import CardListStore from './CardListStore.js'
@@ -7,36 +7,39 @@ function CardListContextProvider ({ children, ...pageProps }) {
   const { storageService } = useContext(globalContext)
   const { commonStore, syncStore, notificationStore } = useContext(globalContext)
 
-  let contextValue
-  if (typeof window !== 'undefined') {
-    contextValue = {
-      cardListStore: new CardListStore(storageService),
-      commonStore,
-      syncStore,
-      notificationStore
+  // creating context
+  const contextValueRef = useRef(null)
+  if (contextValueRef.current === null) {
+    if (typeof window !== 'undefined') {
+      contextValueRef.current = {
+        cardListStore: new CardListStore(storageService),
+        commonStore,
+        syncStore,
+        notificationStore
+      }
+    } else {
+      contextValueRef.current = {
+        cardListStore: new CardListStore(null),
+        commonStore
+      }
     }
-  } else {
-    contextValue = {
-      cardListStore: new CardListStore(null),
-      commonStore
+
+    /**
+     * Initialize predefined data.
+     */
+    if (pageProps.cardListStore) {
+      contextValueRef.current.cardListStore = Object.assign(
+        contextValueRef.current.cardListStore, pageProps.cardListStore)
     }
-  }
 
-  /**
-   * Initialize predefined data.
-   */
-  if (pageProps.cardListStore) {
-    contextValue.cardListStore = Object.assign(
-      contextValue.cardListStore, pageProps.cardListStore)
-  }
-
-  if (pageProps.commonStore) {
-    contextValue.commonStore = Object.assign(
-      contextValue.commonStore, pageProps.commonStore)
+    if (pageProps.commonStore) {
+      contextValueRef.current.commonStore = Object.assign(
+        contextValueRef.current.commonStore, pageProps.commonStore)
+    }
   }
 
   return (
-    <cardListContext.Provider value={contextValue}>
+    <cardListContext.Provider value={contextValueRef.current}>
       {children}
     </cardListContext.Provider>
   )
