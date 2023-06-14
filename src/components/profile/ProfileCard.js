576565b00d45e34ac11174ac6793ab0ab7ba2fd0
Andrey Rychkov <jonny@wholegroup.com>
2023-06-14 10:46:54 +0700

    move app version to profile page

--- src/components/profile/ProfileCard.js
+++ src/components/profile/ProfileCard.js
@@ -1,11 +1,20 @@
 import ProfileCardUser from './ProfileCardUser.js'
 import ProfileCardAnonymous from './ProfileCardAnonymous.js'
+import styles from './ProfileCard.module.css'
 
 function ProfileCard () {
   return (
     <div style={{ textAlign: 'center' }}>
       <ProfileCardAnonymous />
       <ProfileCardUser />
+      <div className={styles.footer}>
+        <div>
+          {new Date().getUTCFullYear()} &copy; AnnualMovies.com
+        </div>
+        <div>
+          v{process.env.NEXT_PUBLIC_MOVIE_VERSION || '00.00.00'}
+        </div>
+      </div>
     </div>
   )
 }
