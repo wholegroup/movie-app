10ede4bbd3dcd61c5dd8b6cea199553ddb2ff612
Andrey Rychkov <jonny@wholegroup.com>
2023-07-14 16:30:40 +0700

    use sw cache for posters

--- cache.js
+++ cache.js
@@ -2,6 +2,36 @@
 
 // original is from @next-pwa/cache.js
 const runtimeCaching = [
+  {
+    urlPattern: /https:\/\/img\.annualmovies\.com\/.+_400\.jpeg$/i,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'posters-preview-v1',
+      fetchOptions: {
+        mode: 'cors' // CacheFirst works only with cors
+      },
+      expiration: {
+        maxEntries: 512,
+        maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
+      }
+    }
+  },
+
+  {
+    urlPattern: /https:\/\/img\.annualmovies\.com\/.+_1185\.jpeg$/i,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'posters-full-v1',
+      fetchOptions: {
+        mode: 'cors' // CacheFirst works only with cors
+      },
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 14 * 24 * 60 * 60 // 14 days
+      }
+    }
+  },
+
   // to support Single Page Application (loading index page for all urls)
   {
     urlPattern: ({ url }) => {
