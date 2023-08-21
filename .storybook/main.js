59c9646930cf3b1f38fe3a0f5c888860a336e9e3
Andrey Rychkov <jonny@wholegroup.com>
2023-08-21 15:45:35 +0400

    add avatar for stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,20 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
   framework: {
-    name: "@storybook/nextjs",
-    options: {},
+    name: '@storybook/nextjs',
+    options: {}
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag'
   },
-};
-export default config;
+  staticDirs: [
+    './static'
+  ]
+}
+export default config
