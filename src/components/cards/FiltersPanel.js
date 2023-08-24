8ce092c2d0ee1ec7bddfe88dfccbbcf6c7385763
Andrey Rychkov <jonny@wholegroup.com>
2023-08-24 14:08:50 +0400

    convert to links

--- src/components/cards/FiltersPanel.js
+++ src/components/cards/FiltersPanel.js
@@ -62,7 +62,7 @@ function FiltersPanel () {
     window.scroll({ top: 0, behavior: 'smooth' })
   }
 
-  const clickReset = () => {
+  const clickReset = (ev) => {
     cardListStore.resetFilters()
       .catch(e => notificationStore.error({ message: e.message }))
     cardListStore.setIsFiltersPanelOpen(false)
@@ -77,38 +77,43 @@ function FiltersPanel () {
         <div>
           <div>Status</div>
           {Object.keys(movieDetailsStatusEnum).map(status => (
-            <div key={status} onClick={() => clickStatus(status)}>
-              {cardListStore.filters.status === status
-                ? <b>{movieDetailsStatusEnum[status]}</b>
-                : movieDetailsStatusEnum[status]}
+            <div key={status}>
+              <a href='javascript:' onClick={() => clickStatus(status)}>
+                {cardListStore.filters.status === status
+                  ? <b>{movieDetailsStatusEnum[status]}</b>
+                  : movieDetailsStatusEnum[status]}
+              </a>
             </div>
           ))}
         </div>
         <div>
           <div>Years</div>
           {cardListStore.years.map(year => (
-            <div key={year} onClick={() => clickYear(year)}>
-              {cardListStore.filters.years.length === 0 || cardListStore.filters.years.includes(year)
-                ? <b>{year}</b>
-                : year}
+            <div key={year}>
+              <a href='javascript:' onClick={() => clickYear(year)}>
+                {cardListStore.filters.years.length === 0 || cardListStore.filters.years.includes(year)
+                  ? <b>{year}</b>
+                  : year}
+              </a>
             </div>
           ))}
         </div>
         <div>
           <div>Genres</div>
           {cardListStore.genres.slice(0, 5).map(genre => (
-            <div key={genre} onClick={() => clickGenre(genre)}>
-              {cardListStore.filters.genres.length === 0 || cardListStore.filters.genres.includes(genre)
-                ? <b>{genre}</b>
-                : genre}
+            <div key={genre}>
+              <a href='javascript:' onClick={() => clickGenre(genre)}>
+                {cardListStore.filters.genres.length === 0 || cardListStore.filters.genres.includes(genre)
+                  ? <b>{genre}</b>
+                  : genre}
+              </a>
             </div>
           ))}
         </div>
       </div>
       <div className={styles.buttons}>
-        <button onClick={() => clickReset()}>
-          Reset
-        </button>
+        {cardListStore.isFiltersModified ? <a href='javascript:' onClick={() => clickReset()}>Reset filters</a> : ''}
+        {!cardListStore.isFiltersModified ? <>&nbsp;</> : ''}
       </div>
     </div>
   )
