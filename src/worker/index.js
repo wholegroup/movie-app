5f451c253520f7a39f322a474ce12f0b90bf8bf6
Andrey Rychkov <jonny@wholegroup.com>
2023-09-03 19:21:18 +0400

    use magic string to avoid including workbox-core

--- src/worker/index.js
+++ src/worker/index.js
@@ -1,9 +1,12 @@
-import { cacheNames } from 'workbox-core'
 import { convertToEmpty } from './convertToEmpty.js'
+
+// use magic string to avoid including 'workbox-core'
+// import { cacheNames } from 'workbox-core'
+const precacheCacheName = `workbox-precache-v2-${self.registration.scope}`
 
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
-    const cache = await caches.open(cacheNames.precache)
+    const cache = await caches.open(precacheCacheName)
     for (const nextRequest of await cache.keys()) {
       if (new URL(nextRequest.url).pathname !== '/') {
         continue
