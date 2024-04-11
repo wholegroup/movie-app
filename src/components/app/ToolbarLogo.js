755b78b26587fdaa3cf8f84f92af211ab1bb2114
Andrey Rychkov <jonny@wholegroup.com>
2024-04-11 18:13:18 +0400

    fix nre in toolbar because of cardListContext dependency

--- src/components/app/ToolbarLogo.js
+++ src/components/app/ToolbarLogo.js
@@ -5,11 +5,11 @@ import { useContext } from 'react'
 import cardListContext from '../cards/cardListContext.js'
 
 function ToolbarLogo () {
-  const { cardListStore } = useContext(cardListContext)
+  const { cardListStore } = useContext(cardListContext) || {}
   return (
     <>
       <noscript>begin:6ae9d088-96e0-4f9c-a709-d1a283163e57</noscript>
-      {cardListStore.cards.length > 0 && (
+      {cardListStore?.cards.length > 0 && (
         <div className={styles.onlyIcon640}>
           <Link href='/about'>
             <img src='/toolbar.png' height='36' alt='logo' />
