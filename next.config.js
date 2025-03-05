d83673c24966c1adcbac508e94bf9d5f0d884731
Andrey Rychkov <jonny@wholegroup.com>
2025-03-05 17:31:22 +0400

    exclude some new files manually

--- next.config.js
+++ next.config.js
@@ -24,12 +24,30 @@ const withPWA = nextPWA({
   navigateFallback: '/',
   navigateFallbackDenylist: [
     /api\/.*/,
-    /noprecache\/.*/,
+    /noprecache\/.*/
   ],
   dynamicStartUrl: false, // if enabled the app fetches index page every time
   reloadOnOnline: false,
   disableDevLogs: true,
   disable: process.env.NODE_ENV === 'development', // temporary disable to suppress logs
+
+  // I have bad-precaching-response of dynamic-css-manifest.json in next.js v15
+  // Probably because of next-pwa is outdated I have to exclude additional file in building precached file list
+  // https://github.com/serwist/serwist/discussions/228#discussioncomment-11864263
+  exclude: [
+    // https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
+    ({ asset }) => {
+      // Add here any file that fails pre-caching
+      const excludeList = [
+        // Default Serwist https://serwist.pages.dev/docs/next/configuring/exclude
+        /\.map$/,
+        /^manifest.*\.js$/,
+        /^server\//,
+        /^(((app-)?build-manifest|react-loadable-manifest|dynamic-css-manifest)\.json)$/
+      ]
+      return excludeList.some(r => r.test(asset.name))
+    }
+  ],
 
   // mode: 'development', // don't minified SW
   // https://github.com/shadowwalker/next-pwa/tree/master/examples/custom-worker
