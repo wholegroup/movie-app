c2c5a8010a359122bf39e7f7795f9792e715c457
Andrey Rychkov <jonny@wholegroup.com>
2026-01-12 11:12:06 +0400

    switch to storybook 10

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,7 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-links'
   ],
   framework: {
     name: '@storybook/nextjs',
