f8518cf778e3ebff39e8a84853443885e91e0f6f
Andrey Rychkov <jonny@wholegroup.com>
2026-01-15 19:01:20 +0400

    refactor runtimeCaching to use Serwist

--- cache.js
+++ cache.js
@@ -1,15 +1,22 @@
-// noinspection RedundantIfStatementJS
+import { CacheFirst, NetworkOnly } from 'serwist'
 
-// original is from @next-pwa/cache.js
 const runtimeCaching = [
+  // do not handle
   {
-    urlPattern: /https:\/\/img\.annualmovies\.com\/.+_400\.jpeg$/i,
-    handler: 'CacheFirst',
-    options: {
+    matcher: /\/noprecache\/.*/,
+    handler: new NetworkOnly()
+  },
+
+  // previews
+  {
+    matcher: /https:\/\/img\.annualmovies\.com\/.+_400\.jpeg$/i,
+    handler: new CacheFirst({
       cacheName: 'posters-preview-v1',
       fetchOptions: {
-        mode: 'cors' // CacheFirst works only with cors
-      },
+        mode: 'cors'
+      }
+    }),
+    options: {
       expiration: {
         maxEntries: 512,
         maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
@@ -17,14 +24,16 @@ const runtimeCaching = [
     }
   },
 
+  // posters
   {
-    urlPattern: /https:\/\/img\.annualmovies\.com\/.+_1185\.jpeg$/i,
-    handler: 'CacheFirst',
-    options: {
+    matcher: /https:\/\/img\.annualmovies\.com\/.+_1185\.jpeg$/i,
+    handler: new CacheFirst({
       cacheName: 'posters-full-v1',
       fetchOptions: {
-        mode: 'cors' // CacheFirst works only with cors
-      },
+        mode: 'cors'
+      }
+    }),
+    options: {
       expiration: {
         maxEntries: 64,
         maxAgeSeconds: 14 * 24 * 60 * 60 // 14 days
@@ -34,18 +43,20 @@ const runtimeCaching = [
 
   // to handle requests to getServerSideProps
   {
-    urlPattern: /\/_next\/data\/.+\/.+\.json(?:\?.+)?$/i,
-    handler: async function () {
-      // shall I add X-Nextjs-Matched-Path: '/' or /[...slug] ?... as NextJS does
-      const emptyJson = { pageProps: {}, __N_SSP: true }
+    matcher: /\/_next\/data\/.+\/.+\.json(?:\?.+)?$/i,
+    handler: async ({ request, url, event }) => {
+      const emptyJson = { pageProps: {}, __N_SSP: true, manual: true }
       const emptyBlob = new Blob([JSON.stringify(emptyJson)], { type: 'application/json' })
-      const emptyOptions = { status: 200, headers: {} }
+      const emptyOptions = {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+      }
       return new Response(emptyBlob, emptyOptions)
     }
   },
 
   // // to support Single Page Application (loading index page for all urls)
-  // // commented in favor of navigateFallback in next.config.js
+  // // commented in favor of navigateFallback in next.config.js (when used next-pwa)
   // {
   //   urlPattern: ({ url }) => {
   //     if (self.origin !== url.origin) {
