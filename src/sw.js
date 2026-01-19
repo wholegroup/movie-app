7b84f161be870ad4b378ed584a40b85772323a0b
Andrey Rychkov <jonny@wholegroup.com>
2026-01-19 11:31:29 +0400

    replace index cleanup logic in service worker with plugin-based approach

--- src/sw.js
+++ src/sw.js
@@ -11,6 +11,26 @@ const serwist = new Serwist({
       /api\/.*/,
       /auth\/.*/,
       /noprecache\/.*/
+    ],
+    plugins: [
+      {
+        // Clean up the index/app page, remove any blocks and leave it blank
+        // it's required to avoid ghosted blinking
+        cacheWillUpdate: async ({ response, request }) => {
+          const url = new URL(request.url)
+          if (url.pathname !== '/') {
+            return response
+          }
+
+          console.log('Cleaning up the index/app page...')
+          const body = await response.text()
+          return new Response(convertToEmpty(body), {
+            headers: response.headers,
+            status: response.status,
+            statusText: response.statusText
+          })
+        }
+      }
     ]
   },
   skipWaiting: true,
@@ -19,64 +39,7 @@ const serwist = new Serwist({
   runtimeCaching: [
     ...runtimeCaching,
     ...defaultCache
-  ],
+  ]
 })
 
 serwist.addEventListeners()
-
-// Clean the index/app page, remove any blocks and leave it blank
-// it's required to avoid ghosted blinking
-const precacheCacheName = serwist.precacheStrategy?.cacheName || '???'
-if (!precacheCacheName) {
-  throw new Error('Precache cache name is not defined.')
-}
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    try {
-      const indexRequest = await waitCleanup(precacheCacheName)
-
-      const cache = await caches.open(precacheCacheName)
-      const indexResponse = await cache.match(indexRequest)
-      if (!indexResponse) {
-        // noinspection ExceptionCaughtLocallyJS
-        throw new Error('Index response undefined')
-      }
-      await cache.put(indexRequest, new Response(
-        convertToEmpty(await indexResponse.text()), {
-          headers: Object.fromEntries(indexResponse.headers)
-        })
-      )
-    } catch (e) {
-      // just in case to avoid breaking worker
-      console.error('Unexpected behaviour!', e)
-    }
-  })())
-})
-
-/**
- * Waits single index request up to 60 seconds and returns it.
- * @param precacheCacheName
- * @returns {Promise<Request>}
- */
-async function waitCleanup (precacheCacheName) {
-  const cache = await caches.open(precacheCacheName)
-
-  // wait until only one index exists after cleanupOutdatedCaches
-  let indexes = []
-  let maxTries = 60 * 2 // because of 2 * 500ms
-  do {
-    indexes = await cache.keys('/', { ignoreSearch: true })
-    // skip 1 second and try again
-    if (indexes.length !== 1) {
-      await new Promise(resolve => setTimeout(resolve, 500))
-    }
-    maxTries--
-  } while (indexes.length !== 1 && maxTries > 0)
-
-  if (indexes.length !== 1) {
-    throw new Error('Couldn\'t find single index page')
-  }
-
-  return indexes[0]
-}
