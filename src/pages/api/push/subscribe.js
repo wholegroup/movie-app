9b5364951dc12ea8e1fbca049c747bad02e1a8f9
Andrey Rychkov <jonny@wholegroup.com>
2026-02-09 12:33:56 +0400

    add `subscribe` API handler for push subscriptions

---
+++ src/pages/api/push/subscribe.js
@@ -0,0 +1,31 @@
+import SyncBackendService from '@/lib/SyncBackendService.js'
+
+export default async function handler (req, res) {
+  const syncService = new SyncBackendService(process.env.MOVIE_APP_USERS_DB)
+  try {
+    await syncService.open({ runMigration: true })
+    await subscribe(req, res, syncService)
+  } finally {
+    if (syncService.isOpen()) {
+      await syncService.close()
+    }
+  }
+}
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ * @param {SyncBackendService} syncService
+ * @returns {Promise<void>}
+ */
+async function subscribe (req, res, syncService) {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST'])
+    res.status(405).send(`Method ${req.method} Not Allowed`)
+    return
+  }
+
+  await syncService.savePush(req.body)
+
+  res.status(200).send('Ok')
+}
