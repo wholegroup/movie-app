5a1a6f932f934a167c6ae3458633e69d1f3d97a7
Andrey Rychkov <jonny@wholegroup.com>
2026-02-23 15:40:06 +0400

    create FiltersPanel stories

---
+++ src/components/cards/FiltersPanel.stories.js
@@ -0,0 +1,50 @@
+import { action } from 'storybook/actions'
+import globalContext from '@/src/context/globalContext.js'
+import CardListStore from '@/src/components/cards/CardListStore.js'
+import StorageService from '@/src/context/StorageService.js'
+import NotificationStore from '@/src/context/NotificationStore.js'
+import FiltersPanel from './FiltersPanel.js'
+import cardListContext from './cardListContext.js'
+
+export default {
+  component: FiltersPanel,
+  decorators: [
+    (Story) => {
+      const notificationStore = Object.assign(new NotificationStore(), {
+        error: action('notificationStore.error')
+      })
+      const storageService = Object.assign(new StorageService(), {
+        getSettings: () => Promise.resolve(null),
+        setSettings: () => Promise.resolve(null),
+      })
+      const cardListStore = Object.assign(new CardListStore(storageService), {
+        cards: [{
+          year: 2020,
+          genres: ['Action', 'Comedy']
+        }, {
+          year: 2021,
+          genres: ['Action', 'Drama']
+        }, {
+          year: 2022,
+          genres: ['Adventure']
+        }, {
+          year: 2023,
+          genres: ['Sci-fi']
+        }, {
+          year: 2024,
+          genres: ['Biography', 'Comedy']
+        }]
+      })
+      return (
+        <globalContext.Provider value={{ storageService }}>
+          <cardListContext.Provider value={{ cardListStore, notificationStore }}>
+            <Story />
+          </cardListContext.Provider>
+        </globalContext.Provider>
+      )
+    },
+    story => <div>{story()}</div>
+  ]
+}
+
+export const Default = {}
