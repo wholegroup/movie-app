159a02fe1b89bd8161b4e705e9be13905422c3e6
Andrey Rychkov <jonny@wholegroup.com>
2026-04-07 17:25:05 +0400

    global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+declare type StorageService = import('@/context/StorageService.js').default
+declare type CommonService = import('@/context/CommonService.js').default
+declare type ApiService = import('@/context/ApiService.js').default
