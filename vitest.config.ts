eb43f35d7026800129da60d4a6c1524f424fda35
Andrey Rychkov <jonny@wholegroup.com>
2026-04-07 16:15:47 +0400

    remove vite-tsconfig-paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
+  plugins: [react()],
+    resolve: {
+        tsconfigPaths: true
+    },
+    test: {
     environment: 'jsdom',
     globals: true
   },
